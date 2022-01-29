5c968556b29a59d810d81a09deb355f89f828078
Ali Shah <s.ashah@outlook.com>
2022-01-28 20:13:49 -0500

    [Finished] challenge 8

--- src/challenges/challenge-08.js
+++ src/challenges/challenge-08.js
@@ -5,4 +5,17 @@
  * E.g: 'Salaam World' => 'alaam WorldS'
  */
 
-module.exports = (s) => (s);
+const rotateString = (s) => s.slice(1) + s.charAt(0);
+const rotateStringReverse = (s) => s.charAt(s.length - 1) + s.slice(0, s.length - 1);
+
+const shift = (s, step = 1) => {
+  const rotationSteps = parseInt(step, 10);
+  const rotateFn = rotationSteps > 0 ? rotateString : rotateStringReverse;
+  let shiftedString = s;
+  for (let i = 0; i < Math.abs(rotationSteps); i += 1) {
+    shiftedString = rotateFn(shiftedString);
+  }
+  return shiftedString;
+};
+
+module.exports = shift;
