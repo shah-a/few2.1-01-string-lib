0c8940840fa39c5080baf04a8ab5184e27619bf7
Ali Shah <s.ashah@outlook.com>
2022-01-28 21:31:40 -0500

    [Renamed] `shift` helper functions

--- src/challenges/challenge-08.js
+++ src/challenges/challenge-08.js
@@ -5,12 +5,12 @@
  * E.g: 'Salaam World' => 'alaam WorldS'
  */
 
-const rotateString = (s) => s.slice(1) + s.charAt(0);
-const rotateStringReverse = (s) => s.charAt(s.length - 1) + s.slice(0, s.length - 1);
+const shiftRight = (s) => s.charAt(s.length - 1) + s.slice(0, s.length - 1);
+const shiftLeft = (s) => s.slice(1) + s.charAt(0);
 
 const shift = (s, step = 1) => {
   const rotationSteps = parseInt(step, 10);
-  const rotateFn = rotationSteps > 0 ? rotateString : rotateStringReverse;
+  const rotateFn = rotationSteps > 0 ? shiftRight : shiftLeft;
   let shiftedString = s;
   for (let i = 0; i < Math.abs(rotationSteps); i += 1) {
     shiftedString = rotateFn(shiftedString);
