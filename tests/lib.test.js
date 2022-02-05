ebc39ce643f830e9e965412abd8455b23da0038b
Ali Shah <s.ashah@outlook.com>
2022-02-05 04:50:18 -0500

    [Finished] adding unit tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,117 +1,103 @@
 const s = require('../src/index');
 const testCases = require('./testCases.json');
 
-describe('Temp', () => {
-  test('temp test', () => {
-    testCases.forEach((testCase) => {
-      expect(s.camelCase(testCase.input)).toBe(testCase.expected.camelCase);
-    });
+function checkCases(funcName, separator) {
+  testCases.forEach((testCase) => {
+    if (funcName.includes('Separator')) {
+      expect(s[funcName](testCase.input, separator)).toBe(testCase.expected[funcName]);
+      return;
+    }
+    if (funcName === 'makeHashtag') {
+      expect(s[funcName](testCase.input)).toEqual(testCase.expected[funcName]);
+      return;
+    }
+    expect(s[funcName](testCase.input)).toBe(testCase.expected[funcName]);
   });
-});
+}
 
 describe('CHALLENGE 1 TESTS', () => {
   test('capFirst', () => {
     expect(s.capFirst('salaam')).toBe('Salaam');
-    expect(s.capFirst('string-with-no-spaces')).toBe('String-with-no-spaces');
-    expect(s.capFirst('Starts with uppercase')).toBe('Starts with uppercase');
-    expect(s.capFirst('the quick brown fox jumps over the lazy dog')).toBe('The quick brown fox jumps over the lazy dog');
-    expect(s.capFirst('   padded   with   spaces   ')).toBe('   padded   with   spaces   ');
-    expect(s.capFirst('')).toBe('');
-    /* eslint-disable no-tabs */
-    expect(s.capFirst(`
-        
-			  `)).toBe(`
-        
-			  `);
-    /* eslint-enable no-tabs */
+    checkCases('capFirst');
   });
 
   test('lowerFirst', () => {
     expect(s.lowerFirst('Salaam')).toBe('salaam');
-    expect(s.lowerFirst('String-with-no-spaces')).toBe('string-with-no-spaces');
-    expect(s.lowerFirst('starts with lowercase')).toBe('starts with lowercase');
-    expect(s.lowerFirst('the quick brown fox jumps over the lazy dog')).toBe('the quick brown fox jumps over the lazy dog');
-    expect(s.lowerFirst('   padded   with   spaces   ')).toBe('   padded   with   spaces   ');
-    expect(s.lowerFirst('')).toBe('');
-    /* eslint-disable no-tabs */
-    expect(s.lowerFirst(`
-        
-			  `)).toBe(`
-        
-			  `);
-    /* eslint-enable no-tabs */
+    checkCases('lowerFirst');
   });
 });
 
 describe('CHALLENGE 2 TESTS', () => {
   test('capWords', () => {
     expect(s.capWords('salaam world')).toBe('Salaam World');
-    expect(s.capWords('one-word')).toBe('One-word');
-    expect(s.capWords('One-word')).toBe('One-word');
-    expect(s.capWords('')).toBe('');
-    /* eslint-disable no-tabs */
-    expect(s.capWords(`
-        
-			  `)).toBe(`
-        
-			  `);
-    /* eslint-enable no-tabs */
+    checkCases('capWords');
   });
 
   test('capTitle', () => {
     expect(s.capTitle('the most foo in bar')).toBe('The Most Foo in Bar');
+    checkCases('capTitle');
   });
 });
 
 describe('CHALLENGE 3 TESTS', () => {
   test('allCaps', () => {
     expect(s.allCaps('salaam world')).toBe('SALAAM WORLD');
+    checkCases('allCaps');
   });
 });
 
 describe('CHALLENGE 4 TESTS', () => {
   test('removeExtraSpaces', () => {
     expect(s.removeExtraSpaces('   Salaam   world!   ')).toBe('Salaam world!');
+    checkCases('removeExtraSpaces');
   });
 });
 
 describe('CHALLENGE 5 TESTS', () => {
   test('kebobCase', () => {
     expect(s.kebobCase(' Kebob Case! ')).toBe('kebob-case!');
+    checkCases('kebobCase');
   });
 
   test('kebobCaseNoSpecial', () => {
     expect(s.kebobCaseNoSpecial(' Kebob Case! ')).toBe('kebob-case');
+    checkCases('kebobCaseNoSpecial');
   });
 });
 
 describe('CHALLENGE 6 TESTS', () => {
   test('snakeCase', () => {
     expect(s.snakeCase(' Snake Case! ')).toBe('snake_case!');
+    checkCases('snakeCase');
   });
 
   test('snakeCaseNoSpecial', () => {
     expect(s.snakeCaseNoSpecial(' Snake Case! ')).toBe('snake_case');
+    checkCases('snakeCaseNoSpecial');
   });
 
   test('customSeparator', () => {
     expect(s.customSeparator(' Custom Case! ', '🐒')).toBe('custom🐒case!');
     expect(s.customSeparator(' Custom Case! ')).toBe('custom case!');
+    checkCases('customSeparator', '🤔');
   });
 
   test('customSeparatorNoSpecial', () => {
     expect(s.customSeparatorNoSpecial(' Custom Case! ', '🐒')).toBe('custom🐒case');
     expect(s.customSeparatorNoSpecial(' Custom Case! ')).toBe('custom case');
+    checkCases('customSeparatorNoSpecial', '🤔');
   });
 });
 
 describe('CHALLENGE 7 TESTS', () => {
   test('camelCase', () => {
     expect(s.camelCase('Camel Case!')).toBe('camelCase!');
+    checkCases('camelCase');
   });
 
   test('camelCaseNoSpecial', () => {
     expect(s.camelCaseNoSpecial('Camel Case!')).toBe('camelCase');
+    checkCases('camelCaseNoSpecial');
   });
 });
 
@@ -120,6 +106,7 @@ describe('CHALLENGE 8 TESTS', () => {
     expect(s.shift('Shift me right by 3', 3)).toBe('y 3Shift me right b');
     expect(s.shift('Shift me left by 3', -3)).toBe('ft me left by 3Shi');
     expect(s.shift('Shift me right by 1')).toBe('1Shift me right by ');
+    checkCases('shift');
   });
 });
 
@@ -127,12 +114,8 @@ describe('CHALLENGE 9 TESTS', () => {
   test('makeHashtag', () => {
     expect(s.makeHashtag('Salaam world')).toEqual(['#Salaam', '#World']);
     expect(s.makeHashtag('Amazing bongo drums for sale')).toEqual(['#Amazing', '#Bongo', '#Drums']);
-    /* eslint-disable no-tabs */
-    expect(s.makeHashtag(`
-        
-			  `)).toEqual([]);
+    checkCases('makeHashtag');
   });
-  /* eslint-enable no-tabs */
 });
 
 describe('CHALLENGE 10 TESTS', () => {
@@ -141,8 +124,9 @@ describe('CHALLENGE 10 TESTS', () => {
     expect(s.isEmpty('  \n  \n\r\t')).toBe(true);
     /* eslint-disable no-tabs */
     expect(s.isEmpty(`
-        
-			  `)).toBe(true);
+
+			`)).toBe(true);
     /* eslint-enable no-tabs */
+    checkCases('isEmpty');
   });
 });
